31ca25a33bed6f7cc4cabcfdbc82c781fa440080
Soloist <947721002@qq.com>
2017-09-12 19:39:41 +0800

    异常测试

---
+++ domain/src/main/java/com/ep/storage/domain/exception/StorageException.java
@@ -0,0 +1,12 @@
+package com.ep.storage.domain.exception;
+
+/**
+ * Created by ly on 2017/9/11
+ */
+public class StorageException extends RuntimeException {
+
+    public StorageException() {
+        super("测试");
+    }
+
+}
